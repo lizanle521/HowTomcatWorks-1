2a659a0258bf332b93a6cc93e79477b7b90c0f59
lizanle <491823689@qq.com>
2018-11-23 09:54:24 +0800

    类名路径修正

--- src/cxs15/pyrmont/digestertest/Test02.java
+++ src/cxs15/pyrmont/digestertest/Test02.java
@@ -12,13 +12,13 @@ public class Test02 {
     File file = new File(path, "employee2.xml");
     Digester digester = new Digester();
     // add rules
-    digester.addObjectCreate("employee", "Employee");
+    digester.addObjectCreate("employee", "cxs15.pyrmont.digestertest.Employee");
     digester.addSetProperties("employee");    
-    digester.addObjectCreate("employee/office", "Office");
+    digester.addObjectCreate("employee/office", "cxs15.pyrmont.digestertest.Office");
     digester.addSetProperties("employee/office");
     digester.addSetNext("employee/office", "addOffice");
     digester.addObjectCreate("employee/office/address", 
-      "Address");
+      "cxs15.pyrmont.digestertest.Address");
     digester.addSetProperties("employee/office/address");
     digester.addSetNext("employee/office/address", "setAddress"); 
     try {
